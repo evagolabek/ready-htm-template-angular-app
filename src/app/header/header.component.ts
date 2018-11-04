761b7922894252331338c8fb42948635d9ac430f
evagolabek <golabek.eva@gmail.com>
2018-11-04 16:10:33 +0100

    separate data header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,8 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
 
+  header = {
+    heading: 'A FREE AND SIMPLE LANDING PAGE',
+    headingtext: 'Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects enjoy!',
+    buttontext: 'START CREATING TODAY',
+    buttonlink: '\home'
+  }
   constructor() { }
 
   ngOnInit() {
