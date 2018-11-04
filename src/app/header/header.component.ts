8eae412894c33669f8a6a45d8d6e48e42cd2c12c
evagolabek <golabek.eva@gmail.com>
2018-11-04 16:16:32 +0100

    configuration file

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,12 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class HeaderComponent implements OnInit {
 
-  header = {
-    heading: 'A FREE AND SIMPLE LANDING PAGE',
-    headingtext: 'Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects enjoy!',
-    buttontext: 'START CREATING TODAY',
-    buttonlink: '\home'
-  }
+  
   constructor() { }
 
   ngOnInit() {
---
+++ src/configuration.ts
@@ -0,0 +1,8 @@
+export const configuration = {
+  header: {
+  heading: 'A FREE AND SIMPLE LANDING PAGE',
+  headingtext: 'Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects enjoy!',
+  buttontext: 'START CREATING TODAY',
+  buttonlink: '\home'
+  },
+};
